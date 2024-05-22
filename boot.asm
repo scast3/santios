d8c64d476059efc879e0f168d8bcbf30dec070a7
Santiago Castillo <scastillo@isengard.mines.edu>
2024-05-21 22:17:33 -0600

    basic loops and alphabet in x86

--- boot.asm
+++ boot.asm
@@ -2,8 +2,15 @@ mov ah, 0x0e
 mov al, 'H'
 int 0x10
 
-jmp $
+loop:
+    add al, 1
+    int 0x10
+    cmp al, 'Z'
+    jne loop
 
+jmp $ ; infinite loop
+; replacing all remaining bits in the boot sector with 0
 times 510-($-$$) db 0
+; boot signature
 db 0x55, 0xaa
 
